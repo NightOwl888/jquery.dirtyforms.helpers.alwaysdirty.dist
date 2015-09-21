11eab5bea3778d67ac8fd0167568667a6f6f9671
MyGet Build Services <noreply@myget.org>
2015-09-21 19:21:24 +0000

    Release version 2.0.0-beta00005

--- jquery.dirtyforms.helpers.alwaysdirty.js
+++ jquery.dirtyforms.helpers.alwaysdirty.js
@@ -1,25 +1,19 @@
 /*!
-Always dirty helper module (for jQuery Dirty Forms) | v2.0.0-beta00004 | github.com/snikch/jquery.dirtyforms
+Always dirty helper module (for jQuery Dirty Forms) | v2.0.0-beta00005 | github.com/snikch/jquery.dirtyforms
 (c) 2015 Mal Curtis
 License MIT
 */
 
 // Example helper, the form is always considered dirty
 
-// Support for UMD: https://github.com/umdjs/umd/blob/master/jqueryPluginCommonjs.js
-// This allows for tools such as Browserify to compose the components together into a single HTTP request.
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else if (typeof exports === 'object') {
-        // Node/CommonJS
-        module.exports = factory(require('jquery'));
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
-}(function ($) {
+(function($, window, document, undefined) {
+    // Can't use ECMAScript 5's strict mode because several apps 
+    // including ASP.NET trace the stack via arguments.caller.callee 
+    // and Firefox dies if you try to trace through "use strict" call chains. 
+    // See jQuery issue (#13335)
+    // Support: Firefox 18+
+    //"use strict";
+
     // Create a new object, with an isDirty method
     var alwaysDirty = {
         isDirty: function (node) {
@@ -29,4 +23,5 @@ License MIT
     };
     // Push the new object onto the helpers array
     $.DirtyForms.helpers.push(alwaysDirty);
-}));
+
+})(jQuery, window, document);
